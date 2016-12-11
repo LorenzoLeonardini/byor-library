7be367996eb066d8899a02acaac4b44ff3ea191e
lorenzodaidone <lorenzodaidoneautomation@gmail.com>
2016-12-11 22:46:30 +0100

    Create Button.ino

---
+++ examples/Basics/Button/Button.ino
@@ -0,0 +1,23 @@
+/*
+ * Button requires a pull-down resistor.
+ * If you place a pull-up resistor, uncomment setup line.
+ */
+
+#include "byor.h"
+
+BYOR byor;
+
+void setup()
+{
+	//byor.setButton1Mode(TOGGLE); // toggle buttons read
+}
+
+void loop()
+{
+	if(byor.button1())
+		Serial.println("Pressed");
+	else
+		Serial.println("Not pressed");
+
+	byor.delay_s(1);
+}
