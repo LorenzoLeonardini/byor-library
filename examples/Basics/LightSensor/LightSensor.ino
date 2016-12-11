9d00b106324196e65fcecfa7e5d01d680d6ea13e
lorenzodaidone <lorenzodaidoneautomation@gmail.com>
2016-12-11 23:01:33 +0100

    Create LightSensor.ino

---
+++ examples/Basics/LightSensor/LightSensor.ino
@@ -0,0 +1,33 @@
+/*
+ * Before run this sketch you have to set two
+ * parameters in byor.h file.
+ * Run first "LightSensorCalibration.ino" file
+ * in examples/Calibration/LightSensorCalibration
+ * and then copy and paste values.
+ */
+
+#include "byor.h"
+
+#define SERIAL_SPEED	9600	// baud rate
+
+BYOR byor;
+
+void setup()
+{
+	Serial.begin(SERIAL_SPEED);
+	byor.setLightSensorMode(PERCENTAGE);
+}
+
+void loop()
+{
+	int light = byor.light();	// read light [%]
+
+	if(light > 70)
+		Serial.println("Very bright");
+	else if(light > 45)
+		Serial.println("Half-light");
+	else
+		Serial.println("Dark");
+
+	byor.delay_s(1);
+}
