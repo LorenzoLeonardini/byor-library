34be55dfdfeb650897fef31d2af96c9e599734d2
Lorenzo Daidone <lorenzodaidoneautomation@gmail.com>
2017-04-24 21:33:08 +0200

    Update Buzzer.ino

--- examples/Basics/Buzzer/Buzzer.ino
+++ examples/Basics/Buzzer/Buzzer.ino
@@ -1,12 +1,25 @@
+/*
+ * Lorenzo Daidone
+ * lorenzodaidone [at] yahoo [dot] it
+ *
+ * Scuola di Robotica
+ * www.scuoladirobotica.it
+ *
+ * Created: 24 April 2017
+ * Last changes: 24 April 2017
+ *
+ * Default buzzer pin: 9
+ */
+
 #include "byor.h"
 #include "pitches.h"
 
-#define QUARTER	500		// quarter duration
+#define QUARTER	0.5		// quarter duration [seconds]
 
 BYOR byor;
 
 // notes in the melody
-int notes[] = {NOTE_E6, NOTE_E6, NOTE_F6, NOTE_G6, NOTE_G6, NOTE_F6, NOTE_E6, NOTE_D6};
+int notes[] = {NOTE_E7, NOTE_E7, NOTE_F7, NOTE_G7, NOTE_G7, NOTE_F7, NOTE_E7, NOTE_D7};
 
 void setup()
 {
@@ -18,8 +31,8 @@ void loop()
 	for(int i = 0; i < 8; i++)
 	{
 	   byor.buzzer(notes[i], QUARTER);
-	   delay(100);
+	   byor.delay_s(0.1); // wait 100 ms
 	}
 	byor.buzzer(NOTE_C6, QUARTER*2);
-	delay(100);
+	byor.delay_s(2.5); // wait 2500 ms
 }
